5b4ef1ea807d8705d5356a7692103cedd9492452
Vilguax <axel.pelassa@hotmail.fr>
2023-11-06 09:02:01 +0100

    ex4-a fonction return ctr

---
+++ ex4-a.py
@@ -0,0 +1,7 @@
+def convert_ctr(impr, clic):
+    return clic / impr
+
+print(convert_ctr(100, 5) * 100)
+print(convert_ctr(1000, 10) * 100)
+print(convert_ctr(248590, 237) * 100)
+print(convert_ctr(2156789230, 219873) * 100)
