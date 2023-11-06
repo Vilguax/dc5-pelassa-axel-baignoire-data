acbb43594bae4391fedbc1aaec7d9a2ff4b3a034
Vilguax <axel.pelassa@hotmail.fr>
2023-11-06 08:38:43 +0100

    ajout de la fonctionnalité de calcul des dépenses totales pour la liste "depenses"

---
+++ ex2-a.py
@@ -0,0 +1,8 @@
+depenses = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000]
+
+total = 0
+for depense in depenses:
+    total += depense
+
+print("Dépenses totales :", total)
+
