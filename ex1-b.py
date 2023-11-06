ac8365d3c1cda91f49275b9887df5932b1f90cc5
Vilguax <axel.pelassa@hotmail.fr>
2023-11-06 11:27:31 +0100

    Correction de la logique des accents, ponctuation et tirets

--- ex1-b.py
+++ ex1-b.py
@@ -1,32 +1,36 @@
 phrase = input("Saisissez une phrase : ")
 nb_mots = 0
-
 precedent_alphanum = False
+precedent_ponctuation = False
+
 for lettre in phrase:
-    if ('a' <= lettre <= 'z') or ('A' <= lettre <= 'Z') or ('0' <= lettre <= '9'):
+    if ('a' <= lettre <= 'z') or ('A' <= lettre <= 'Z') or ('0' <= lettre <= '9') or lettre in "àâäéèêëïîôöùûüç":
         if not precedent_alphanum:
             nb_mots += 1
         precedent_alphanum = True
+        precedent_ponctuation = False 
     else:
-        if lettre == " ":
-            precedent_alphanum = False
+        if lettre in "'-!.?,":
+            if not precedent_ponctuation:
+                nb_mots += 1
+            precedent_ponctuation = True
         else:
-            if not precedent_alphanum:
-                nb_mots += 1 
-            precedent_alphanum = False
+            precedent_ponctuation = False
+        precedent_alphanum = False
 
 phrase_maj = ""
 for lettre in phrase:
-    if lettre >= "a" and lettre <= "z":
-        lettre = chr(ord(lettre) - 32)
-    phrase_maj += lettre
+    if 'a' <= lettre <= 'z':
+        phrase_maj += chr(ord(lettre) - 32)
+    else:
+        phrase_maj += lettre
 
 phrase_min = ""
 for lettre in phrase:
-    if lettre >= "A" and lettre <= "Z":
-        lettre = chr(ord(lettre) + 32)
-    phrase_min += lettre
-
+    if 'A' <= lettre <= 'Z':
+        phrase_min += chr(ord(lettre) + 32)
+    else:
+        phrase_min += lettre
 
 print("Nombre de mots :", nb_mots)
 print("Phrase en majuscule :", phrase_maj)
