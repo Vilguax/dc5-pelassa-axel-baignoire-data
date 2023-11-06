6366ddd2aa7d6bd444700cb6c89550c50b23b8d1
Vilguax <axel.pelassa@hotmail.fr>
2023-11-06 08:25:42 +0100

    ex1-b Input phrase, sortie maj, minuscule et compteur de mots

---
+++ ex1-b.py
@@ -0,0 +1,4 @@
+phrase = input("Saisissez une phrase : ")
+print("La phrase en majuscules : ", phrase.upper())
+print("La phrase en minuscules : ", phrase.lower())
+print("Le nombre de mots : ", len(phrase.split()))
