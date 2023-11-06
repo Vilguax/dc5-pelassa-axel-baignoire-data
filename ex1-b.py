6ca85b6bea377d0e5ea634136e02690be233b871
Vilguax <axel.pelassa@hotmail.fr>
2023-11-06 08:33:10 +0100

    ex1-b sans utiliser upper, lower, split

--- ex1-b.py
+++ ex1-b.py
@@ -1,4 +1,23 @@
 phrase = input("Saisissez une phrase : ")
-print("La phrase en majuscules : ", phrase.upper())
-print("La phrase en minuscules : ", phrase.lower())
-print("Le nombre de mots : ", len(phrase.split()))
+nb_mots = 0
+for lettre in phrase:
+    if lettre == " ":
+        nb_mots += 1
+nb_mots += 1
+
+phrase_maj = ""
+for lettre in phrase:
+    if lettre >= "a" and lettre <= "z":
+        lettre = chr(ord(lettre) - 32)
+    phrase_maj += lettre
+
+phrase_min = ""
+for lettre in phrase:
+    if lettre >= "A" and lettre <= "Z":
+        lettre = chr(ord(lettre) + 32)
+    phrase_min += lettre
+
+
+print("Nombre de mots :", nb_mots)
+print("Phrase en majuscule :", phrase_maj)
+print("Phrase en minuscule :", phrase_min)
